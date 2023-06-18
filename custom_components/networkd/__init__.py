e38735ef84fec5e031b9cb79698aa04a5fd2c7da
Klaas Winter <dev@klaaswinter.com>
2023-06-18 20:33:09 +0200

    Use async_forward_entry_setups for 2023.3

--- custom_components/networkd/__init__.py
+++ custom_components/networkd/__init__.py
@@ -21,7 +21,7 @@ async def async_setup_entry(hass, entry):
 
     await hass.data[DOMAIN].coordinator.async_config_entry_first_refresh()
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
